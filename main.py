81467ce9db8aa6b5a3902fe4f66bee67eeca4eb9
Sulankovski <sulankovski02@gmail.com>
2023-11-16 17:33:37 +0100

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
+from pathlib import Path
+
 import torch
 import torch.utils.data
 
+from models import TemporalGNN
 from train_and_test import train_and_eval_DCRNN, train_and_eval_TGCN, train_and_eval_TGNN
 
 DEVICE: str = "cuda" if torch.cuda.is_available() else "cpu"
 
 if __name__ == "__main__":
-    print(DEVICE)
-    train_and_eval_TGCN(number_of_epochs=5, BATCH_SIZE=1)
-    
-    print("bla bla")
-    print("bla bla1")
 
-    print("fdsdfg")
-    print("bla bla 2")
+    path_TGNN = Path(r'saved_models/model_TGNN.pth')
+
+    model_TGNN = TemporalGNN(
+        node_features=2,
+        out_periods=12,
+        hidden_units=32,
+        batch_size=32,
+    ).to(DEVICE)
+
+    if path_TGNN.is_file():
+        model_TGNN = torch.load(path_TGNN)
+        print("TGNN device found !")
+    else:
+        train_and_eval_TGNN(number_of_epochs=2, BATCH_SIZE=32)
