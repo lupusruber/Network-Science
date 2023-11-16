ff0c6f35f3bd4472aeefd352ffdbe460f4b46144
Sulankovski <sulankovski02@gmail.com>
2023-11-16 16:57:01 +0100

    main

--- main.py
+++ main.py
@@ -8,5 +8,3 @@ DEVICE: str = "cuda" if torch.cuda.is_available() else "cpu"
 if __name__ == "__main__":
     print(DEVICE)
     train_and_eval_TGCN(number_of_epochs=5, BATCH_SIZE=1)
-    
-    print("Test for repo control")
