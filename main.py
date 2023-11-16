ed4db2322827cf65bfe5226ae5788704b7114873
petarmalinovski <petarpro234@gmail.com>
2023-11-16 16:59:24 +0100

    Changed main

--- main.py
+++ main.py
@@ -8,3 +8,5 @@ DEVICE: str = "cuda" if torch.cuda.is_available() else "cpu"
 if __name__ == "__main__":
     print(DEVICE)
     train_and_eval_TGCN(number_of_epochs=5, BATCH_SIZE=1)
+    
+    print("bla bla")
