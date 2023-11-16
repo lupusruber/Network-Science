f887e5def5c38118bfe3c1d493904c07f46dec42
petarmalinovski <petarpro234@gmail.com>
2023-11-16 18:25:00 +0100

    Get saved model

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ DEVICE: str = "cuda" if torch.cuda.is_available() else "cpu"
 
 if __name__ == "__main__":
 
-    path_TGNN = Path(r'saved_models/model_TGNN.pth')
+    path_TGNN = Path(r'saved_models/model_TGNN_state_dict.pth')
 
     model_TGNN = TemporalGNN(
         node_features=2,
@@ -20,7 +20,9 @@ if __name__ == "__main__":
     ).to(DEVICE)
 
     if path_TGNN.is_file():
-        model_TGNN = torch.load(path_TGNN)
+        loaded_state_dict = torch.load(path_TGNN)
+        model_TGNN.load_state_dict(loaded_state_dict)
         print("TGNN device found !")
     else:
-        train_and_eval_TGNN(number_of_epochs=2, BATCH_SIZE=32)
+        state_dict = train_and_eval_TGNN(number_of_epochs=2, BATCH_SIZE=32)
+        torch.save(state_dict, path_TGNN)
