d526e8d96aec0b134663f36ff37de06fd2eaa364
Sulankovski <sulankovski02@gmail.com>
2023-12-26 17:27:40 +0100

    sredeno

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import torch.utils.data
-from viz import visualise_sensors
+from viz import get_all_y_for_DCRNN, visualise_sensors
 
 DEVICE: str = "cuda" if torch.cuda.is_available() else "cpu"
 
 if __name__ == "__main__":
-    visualise_sensors("TGNN")
+    # visualise_sensors("DCRNN", "TGCN", "TGNN")
+    get_all_y_for_DCRNN()
