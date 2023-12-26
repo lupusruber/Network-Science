b8aa58bfb49b25fdfd1600556611203a83180e34
petarmalinovski <petarpro234@gmail.com>
2023-12-26 20:22:26 +0100

    test

--- graph.py
+++ graph.py
@@ -26,14 +26,13 @@ def graph_info_and_visualizations(graph: nx.Graph) -> None:
     plt.show()
 
 
-def create_weighted_graph(y_true: Tensor, graph: nx.Graph, source: int, target: int):
-    predictions = y_true
+def create_weighted_graph(predictions: Tensor, graph: nx.Graph, source: int, target: int):
     node_labels = {
         node: prediction for node, prediction in zip(graph.nodes, predictions)
     }
 
     edge_weights = {
-        (u, v): 1 / prediction for (u, v), prediction in zip(graph.edges, predictions)
+        (u, v): 1 / prediction if prediction != 0 else 100 for (u, v), prediction in zip(graph.edges, predictions)
     }
 
     # Set the node labels and edge weights of the graph
@@ -52,27 +51,26 @@ def create_weighted_graph(y_true: Tensor, graph: nx.Graph, source: int, target: 
         "red" if edge in zip(shortest_path, shortest_path[1:]) else "black"
         for edge in graph.edges
     ]
-    node_colors = ["green" if node in shortest_path else "gray" for node in graph.nodes]
+    node_colors = ["green" if node in shortest_path else "grey" for node in graph.nodes]
 
     return shortest_path, node_colors, edge_colors
 
 
 if __name__ == "__main__":
-    net = Network(notebook=False)
+    net = Network(notebook=False, neighborhood_highlight=True)
 
     timestep = 1
     scaler = MinMaxScaler(feature_range=(0, 100))
     y_pred, y_true = get_all_y_for_DCRNN()
     y_pred = y_pred[timestep, :, 0]
-    print(y_pred.shape)
-    y_pred = scaler.fit_transform([y_pred])
+    y_pred = scaler.fit_transform([y_pred])[0]
 
     shortest_path, node_colors, edge_colors = create_weighted_graph(
-        y_pred, graph, source=10, target=7
+        y_pred, graph, source=10, target=300
     )
 
-    for node, color in zip(graph.nodes, node_colors):
-        net.add_node(node, color=color)
+    for index, (node, color) in enumerate(zip(graph.nodes, node_colors)):
+        net.add_node(node, color=color, label=index)
 
     for edge, color in zip(graph.edges, edge_colors):
         net.add_edge(*edge, color=color)
